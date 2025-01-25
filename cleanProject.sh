7f7821923a511d48f5f9e1de841cb6c8999c0990
Arjuna1978 <99155024+Arjuna1978@users.noreply.github.com>
2025-01-25 12:36:46 +1100

    Update cleanProject.sh

--- cleanProject.sh
+++ cleanProject.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#This simple script will help you clean your react-native project. this script is useful when 
+#  (1) To fix the React Native Mistmatch error)
+#  (2) Your project worked previously, but not in a different React Native version
+#  (3) Package versions in the package.json file is broken
+#  (4) Something is wrong with your ios/Podfile
+#  (5) You have opened the metro bundler in the wrong folder
+#  (6) You have changed your codebase to point to a different Firebase project
+#  (7) You are encountering build issues in Xcode or Android Studio
+
+
 echo "Resetting your project may take a moment"
 
 echo "Clearing watchman"
