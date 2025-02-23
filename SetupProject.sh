f5858536dd8c0a151535e3e77273605a0c7ba538
Arjuna1978 <99155024+Arjuna1978@users.noreply.github.com>
2025-02-23 16:42:06 +1100

    Updated Setup project to make it more generic

--- SetupProject.sh
+++ SetupProject.sh
@@ -3,48 +3,28 @@
 #setting up project using NPX
 
 read -p "Enter project name: " ProjectName
-npm uninstall -g react-native-cli @react-native-community/cli
-npx @react-native-community/cli init $ProjectName
-cd $ProjectName
-
+npx @react-native-community/cli@latest init $ProjectName
 # Creating a relatively standard directory structure
+cd $ProjectName
 echo "Setting up project directory structure"
 mkdir src
-cd src
-mkdir images
-mkdir components
-cd components
-mkdir common
-mkdir pages
-cd common
-mkdir hooks
-cd ..
-cd pages
-mkdir home
-cd ..
-cd ..
-cd ..
-
-echo "moving App.tsx to the src folder"
-# mv ./App.tsx ./src
+mkdir ./src/images
+mkdir ./src/components
+mkdir ./src/pages
+mkdir ./src/hooks
 
 echo "setting up local.properties"
-cd android
-touch local.properties
-
-# getting current username
-myUserName="$(whoami)"
-
-echo "sdk.dir = /Users/$myUserName/Library/Android/sdk" >> local.properties
+echo "setting up local.properties"
+touch ./android/local.properties
+echo "sdk.dir = /Users/arjunavijayanayam/Library/Android/sdk" >> ./android/local.properties
 echo "cleaning Android Gradle"
-./gradlew clean
+cd android
+bash gradlew clean
 cd ..
-
 echo "updating pods"
 gem install cocoapods
 cd ios
 pod install
 pod install --repo-update
 cd ..
-cd android
-cd ..
+
